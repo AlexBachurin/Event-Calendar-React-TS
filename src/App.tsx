77ec64d174f4407d365a995334673b6c32640d0c
Alex <alexbachurin91@gmail.com>
2022-04-02 07:42:32 +0300

    navigate to event page after auth fix + dont logout on page reload

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,33 @@ import { useNavigate } from 'react-router-dom';
 import './App.css';
 import AppRouter from './components/AppRouter';
 import Navbar from './components/Navbar';
+import { useAppDispatch } from './hooks/useAppDispatch';
 import { useTypedSelector } from './hooks/useTypedSelector';
+import { iUser } from './models/IUser';
+import { AuthActionCreators } from './store/reducers/auth/action-creators';
 
 function App() {
-
+  const dispatch = useAppDispatch();
   //navigate to event page after successfull login
   let navigate = useNavigate();
   const {isAuth} = useTypedSelector(state => state.authReducer);
   useEffect(() => {
-      navigate('/event')
+      if (isAuth) {
+        navigate('/event')
+      }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isAuth])
+
+  //Prevent logout on page reload, check local storage for auth, if we have it, then get it and set user
+  useEffect(() => {
+    const auth = localStorage.getItem('auth');
+  
+    if (auth) {
+      dispatch(AuthActionCreators.setIsAuth(true));
+      dispatch(AuthActionCreators.setUser({username: localStorage.getItem('username' || '')} as iUser))
+    }
+  }, [])
+  
   
   return (
     <Layout>
